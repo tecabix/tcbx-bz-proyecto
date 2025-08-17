6e6af165f96bcb089b3243d8272f42ddf4c2f2ca
Angel Abinadi Ramirez Urrutia <abinadi.ramirez.urrutia@tecabix.com>
2025-08-17 10:20:19 -0600

    feat DEV-1721: Se agrega detalle del proyecto

---
+++ src/main/java/com/tecabix/bz/proyecto/Proyecto003BZ.java
@@ -0,0 +1,81 @@
+package com.tecabix.bz.proyecto;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+
+import com.tecabix.db.entity.PersonaFisica;
+import com.tecabix.db.entity.Proyecto;
+import com.tecabix.db.entity.Usuario;
+import com.tecabix.db.repository.PersonaFisicaRepository;
+import com.tecabix.db.repository.ProyectoRepository;
+import com.tecabix.db.repository.UsuarioRepository;
+import com.tecabix.res.a.RSA026;
+import com.tecabix.res.b.RSB032;
+import com.tecabix.sv.rq.RQSV040;
+
+public class Proyecto003BZ {
+
+	private ProyectoRepository proyectoRepository;
+	private UsuarioRepository usuarioRepository;
+	private PersonaFisicaRepository personaFisicaRepository;
+	
+	public ResponseEntity<RSB032> detalle(final RQSV040 rqsv040) {
+		RSB032 respose = rqsv040.getRsb032();
+		String ticket = rqsv040.getTicket();
+		long id = Long.parseLong(ticket.replace("DEV-", ""));
+		Optional<Proyecto> proyectoOp = proyectoRepository.findById(id);
+		if(proyectoOp.isEmpty()) {
+			return respose.notFound("No se encontro el registro");
+		}
+		Proyecto proyecto = proyectoOp.get();
+		Map<Byte, String> nombres = new HashMap<Byte, String>();
+		PersonaFisica persona = proyecto.getTrabajador().getPersonaFisica();
+		String nombre = persona.getNombre();
+		if(persona.getApellidoPaterno() != null && persona.getApellidoMaterno() != null) {
+			if(!persona.getApellidoPaterno().isBlank() && !persona.getApellidoMaterno().isBlank()) {
+				nombre = nombre + " " + persona.getApellidoPaterno() + " " + persona.getApellidoMaterno();
+			}
+		}
+		nombres.put(RSA026.RESPONSABLE, nombre);
+		
+		Optional<Usuario> usuarioOptional = usuarioRepository.findById(proyecto.getUsuarioCreador());
+		if(usuarioOptional.isEmpty()) {
+			return respose.notFound("No se encontro el usuario que creo el ticket");
+		}
+		
+		Optional<PersonaFisica> personaFisicaOP = personaFisicaRepository.findByPersona(usuarioOptional.get().getUsuarioPersona().getPersona().getId());
+		if(personaFisicaOP.isEmpty()) {
+			return respose.notFound("No se encontro la persona que creo el ticket");
+		}
+		persona = personaFisicaOP.get();
+		nombre = persona.getNombre();
+		if(persona.getApellidoPaterno() != null && persona.getApellidoMaterno() != null) {
+			if(!persona.getApellidoPaterno().isBlank() && !persona.getApellidoMaterno().isBlank()) {
+				nombre = nombre + " " + persona.getApellidoPaterno() + " " + persona.getApellidoMaterno();
+			}
+		}
+		nombres.put(RSA026.CREADOR, nombre);
+		
+		usuarioOptional = usuarioRepository.findById(proyecto.getIdUsuarioModificado());
+		if(usuarioOptional.isEmpty()) {
+			return respose.notFound("No se encontro el usuario que modifico el ticket");
+		}
+		
+		personaFisicaOP = personaFisicaRepository.findByPersona(usuarioOptional.get().getUsuarioPersona().getPersona().getId());
+		if(personaFisicaOP.isEmpty()) {
+			return respose.notFound("No se encontro la persona que modifico el ticket");
+		}
+		persona = personaFisicaOP.get();
+		nombre = persona.getNombre();
+		if(persona.getApellidoPaterno() != null && persona.getApellidoMaterno() != null) {
+			if(!persona.getApellidoPaterno().isBlank() && !persona.getApellidoMaterno().isBlank()) {
+				nombre = nombre + " " + persona.getApellidoPaterno() + " " + persona.getApellidoMaterno();
+			}
+		}
+		nombres.put(RSA026.MODIFICADOR, nombre);
+		return respose.ok(proyecto, nombres);
+	}
+}
